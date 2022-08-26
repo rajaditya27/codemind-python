b0b847b27a6c63dd361e9f5dd38d6c711fdbb9cc
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-08-26 23:31:22 +0530

    n=int(input()) l=list(map(int,input().split())) z=int(input()) c=0 for i in range(n):     if l[i]==z:         c+=1 # else:     # print(0) print(c)

--- count_of_given_element_in_an_array.py
+++ count_of_given_element_in_an_array.py
@@ -1,4 +1,10 @@
 n=int(input())
-arr=list(map(int,input().split()))
-b=int(input())
-print(arr.count(b))
+l=list(map(int,input().split()))
+z=int(input())
+c=0
+for i in range(n):
+    if l[i]==z:
+        c+=1
+# else:
+    # print(0)
+print(c)
