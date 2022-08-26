4a7089ff105f9b06853462c684007190357d7e5c
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-08-26 23:22:09 +0530

    n=int(input()) l=list(map(int,input().split())) x=[] for i in range(n):     if l[i]%2==0:         e=i print(e)

--- Last_even_element_index.py
+++ Last_even_element_index.py
@@ -1,6 +1,7 @@
 n=int(input())
-arr=list(map(int,input().split()))
-for i in range (n):
-    if arr[i]%2==0:
+l=list(map(int,input().split()))
+x=[]
+for i in range(n):
+    if l[i]%2==0:
         e=i
 print(e)
