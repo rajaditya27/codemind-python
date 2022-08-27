d032de786f0b694593ae365d806e4651669980d3
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-08-28 00:29:01 +0530

    n=int(input()) l=list(map(int,input().split())) if l[n-1]!=l[n-2]+l[n-3]:     print('no') else:     for i in range(n-2):         if l[i]+l[i+1]!=l[i+2]:             print('no')             break     else:         print('yes')

--- Fibonacci_array.py
+++ Fibonacci_array.py
@@ -1,13 +1,11 @@
 n=int(input())
 l=list(map(int,input().split()))
-c=0
-d=0
 if l[n-1]!=l[n-2]+l[n-3]:
-    print("no")
+    print('no')
 else:
     for i in range(n-2):
         if l[i]+l[i+1]!=l[i+2]:
-            print("no")
+            print('no')
             break
     else:
-        print("yes")
+        print('yes')
