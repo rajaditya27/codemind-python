1125341216426a91c49272d32e1f454603997988
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-08-27 10:01:09 +0530

    def prime(n):     if n==1:         return 0     elif n==2:         return 1     for i in range(2,int(n**0.5)+2):         if n%i==0:             return 0     return 1 n=int(input()) a=list(map(int,input().split())) c=0 # for i in a: ma = a.index(max(a)) mi = a.index(min(a)) # c = 0 if mi<ma:     for i in range(mi,ma+1):         if prime(a[i]):             c+=1     print(c) else:     for i in range(ma,mi+1):         if prime(a[i]):             c+=1     print(c)

--- Primes_between_min_and_max.py
+++ Primes_between_min_and_max.py
@@ -1,24 +1,26 @@
 def prime(n):
     if n==1:
-        return False
-    for i in range(2,int(n**.5)+1):
+        return 0
+    elif n==2:
+        return 1
+    for i in range(2,int(n**0.5)+2):
         if n%i==0:
-            return False
-    return True
-    
+            return 0
+    return 1
 n=int(input())
-l=list(map(int,input().split()))
+a=list(map(int,input().split()))
 c=0
-ma=l.index(max(l))
-mi=l.index(min(l))
+# for i in a:
+ma = a.index(max(a))
+mi = a.index(min(a))
+# c = 0
 if mi<ma:
     for i in range(mi,ma+1):
-        if prime(l[i]):
+        if prime(a[i]):
             c+=1
     print(c)
 else:
     for i in range(ma,mi+1):
-        if prime(l[i]):
+        if prime(a[i]):
             c+=1
     print(c)
-
