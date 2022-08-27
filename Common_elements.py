20780f7c155da95f3dd0d219eb1b657ac4ab45b9
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-08-28 00:01:29 +0530

    a,b=map(int,input().split()) n=list(map(int,input().split())) m=list(map(int,input().split())) # n=set(n) # m=set(m) c=[] # g=[] for i in n:     if i in m and i not in c:         c.append(i) # for i in m: #     if i in n: #         c.append(i) print(*c)

--- Common_elements.py
+++ Common_elements.py
@@ -1,10 +1,14 @@
 a,b=map(int,input().split())
-l=list(map(int,input().split()))
-k=list(map(int,input().split()))
-# l=set(l)
-# k=set(k)
-g=[]
-for i in l:
-    if i in k and i not in g:
-        g.append(i)
-print(*g)
+n=list(map(int,input().split()))
+m=list(map(int,input().split()))
+# n=set(n)
+# m=set(m)
+c=[]
+# g=[]
+for i in n:
+    if i in m and i not in c:
+        c.append(i)
+# for i in m:
+#     if i in n:
+#         c.append(i)
+print(*c)
