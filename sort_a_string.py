3eb843451bb3841779857aa18d21ea826de914b4
Aditya Raj <99981434+rajaditya27@users.noreply.github.com>
2022-08-30 03:15:31 +0530

    a=input().split() b=[] c=[] for i in a:     for j in i:         if j.isalpha():             b.append(j)         else:             c.append(j) b.sort() k,l=0,0 for i in a:     for j in range(len(i)):         if i[j] in b:             print(b[k],end='')             k+=1         else:             print(c[l],end='')             l+=1     print(end=' ')

---
+++ sort_a_string.py
@@ -0,0 +1,20 @@
+a=input().split()
+b=[]
+c=[]
+for i in a:
+    for j in i:
+        if j.isalpha():
+            b.append(j)
+        else:
+            c.append(j)
+b.sort()
+k,l=0,0
+for i in a:
+    for j in range(len(i)):
+        if i[j] in b:
+            print(b[k],end='')
+            k+=1
+        else:
+            print(c[l],end='')
+            l+=1
+    print(end=' ')
